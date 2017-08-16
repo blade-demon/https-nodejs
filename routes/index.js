d7af6627af8ece960ea2ec1b9380f9e9dd7c975c
blade-demon <949659359@qq.com>
2017-08-16 10:22:32 +0800

    fix bugs

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,21 @@ require('body-parser-xml')(bodyParser);
 var fs = require('fs');
 var request = require('request');
 var specialRequest = request.defaults({
-    strictSSL: false
+  agentOptions: {
+    ca: fs.readFileSync('ssl/COMODOECCDomainValidationSecureServerCA.crt')
+    // ca: fs.readFileSync('ssl/COMODOECCAddTrustCA.crt')
+  }
 });
 var inLicense = fs.readFileSync('./license.txt', 'utf-8');
 var router = express.Router();
 
 
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('index', {title: 'Express'});
 });
 
 
-router.post('/verify',bodyParser.urlencoded({extended:false}),function(req,res){
+router.post('/verify', bodyParser.urlencoded({extended: false}), function (req, res) {
   var input = req.body;
   var gmsfhm = input.rows.row[1].gmsfhm;
   var xm = input.rows.row[1].xm;
@@ -28,34 +31,34 @@ router.post('/verify',bodyParser.urlencoded({extended:false}),function(req,res){
   */
   var url = "https://ws.nciic.org.cn/nciic_ws/services/NciicServices?wsdl";
   var args = {
-    gmsfhm:gmsfhm,
-    xm:xm
+    gmsfhm: gmsfhm,
+    xm: xm
   };
 
   soap.createClient(url, {
-      // wsdl_headers: {Authorization: auth},
-      request : specialRequest
-  }, function(err,client){
-    if(err) {
-        console.error("error:", err);
-        res.status(500).send(err);
+    wsdl_headers: {Authorization: 'auth'},
+    request: specialRequest
+  }, function (err, client) {
+    if (err) {
+      console.error("error:", err);
+      res.status(500).send(err);
     }
     else {
       client.nciicCheck({
         inLicense: inLicense,
         inConditions: '<?xml version="1.0" encoding="utf-8"?>' +
-          '<ROWS><INFO><SBN>上海星游纪信息技术有限公司</SBN></INFO>' +
-          '<ROW><GMSFHM>公民身份号码</GMSFHM>' +
-          '<XM>姓名</XM></ROW>' +
-          '<ROW FSD="200333" YWLX="是否年满18周岁">' +
-          '<GMSFHM>320923198909300019</GMSFHM>' +
-          '<XM>徐紫微</XM></ROW></ROWS>'
-      },function(err,response){
-        if(err) {
-            console.log(err);
-            res.status(500).send(err);
+        '<ROWS><INFO><SBN>上海星游纪信息技术有限公司</SBN></INFO>' +
+        '<ROW><GMSFHM>公民身份号码</GMSFHM>' +
+        '<XM>姓名</XM></ROW>' +
+        '<ROW FSD="200333" YWLX="是否年满18周岁">' +
+        '<GMSFHM>320923198909300019</GMSFHM>' +
+        '<XM>徐紫微</XM></ROW></ROWS>'
+      }, function (err, response) {
+        if (err) {
+          console.log(err);
+          res.status(500).send(err);
         }
-        else{
+        else {
           res.status(200).send(response);
         }
       })
