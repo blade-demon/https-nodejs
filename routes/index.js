7d64680aed32afc4bb0e85f680786e91d5b0719a
blade-demon <949659359@qq.com>
2017-08-17 10:35:17 +0800

    fix bugs

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ require('https').globalAgent.options.ca = rootCas;
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('index', {title: 'Express'});
+    res.render('index');
 });
 
 router.post('/verify', function (req, res) {
